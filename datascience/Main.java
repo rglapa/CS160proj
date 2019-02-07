850994bbe18aeb696d1714dd452a9d9505758e28
rglapa <ruben.glapa@icloud.com>
2019-02-07 12:07:31 -0800

    Started working on Main.java

--- datascience/Main.java
+++ datascience/Main.java
@@ -1,12 +1,13 @@
 package datascience;
 
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello this is main");
-		
+
 		Tokenizer token =new Tokenizer();
-		token.sayHello();
+		token.readTokens2(args[0]);
 	}
 }
